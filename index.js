8ac71622226cffc907f1d82ca03445ccc30e9134
James Gomis <jamesislike@gmail.com>
2017-11-18 13:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return `This one is on me!`
+  } else if (ride > 2000 && ride < 2500){
+    return `I will gladly take your thirty bucks.`
+  } else {
+    return `No can do.`
+  }
+}
+
+
+// //function ternaryCheckCity(destination){
+//   if (destination
+// }
+//
+//
+// function switchOnCharmFromTip(tip){
+//
+// }
